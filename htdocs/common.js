3c773af528bfaf51836cec22dff203d902176fb3
NoguchiEikichi <aymek.5aikichi@icloud.com>
2024-09-11 12:10:29 +0900

    フッターの調整

--- htdocs/common.js
+++ htdocs/common.js
@@ -14,10 +14,11 @@ document.querySelectorAll('a[href^="#"]').forEach(anchor => {
 
 //ヘッダーの読み込み
 fetch('../reference/header.html')
-.then(response => response.text())
-.then(data => document.getElementById('main-header').innerHTML = data);
+    .then(response => response.text())
+    .then(data => document.getElementById('main-header').innerHTML = data);
 
-//フッターの読み込み
+// フッターの読み込み
 fetch('../reference/footer.html')
-.then(response => response.text())
-.then(data => document.getElementById('main-footer').innerHTML = data);
+    .then(response => response.text())
+    .then(data => document.getElementById('main-footer').innerHTML = data)
+    .catch(error => console.error('フッターの読み込みに失敗しました:', error));
